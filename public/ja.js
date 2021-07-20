48fd81051701482910244d44eaf7fc35b5062366
Ambrus <37315575+dkmmtwtw@users.noreply.github.com>
2021-07-20 15:52:51 +0200

    mhm

--- public/ja.js
+++ public/ja.js
@@ -1,5 +1,11 @@
 XMLHttpRequest.prototype.setRequestHeaders = function (obj) {
-			Object.keys(obj).forEach(key => {
-				this.setRequestHeader(key, obj[key]);
-			});
-		}
+	Object.keys(obj).forEach(key => {
+		this.setRequestHeader(key, obj[key]);
+	});
+}
+function startsWithVowel(szoveg){
+    return 'aeiou'.includes(szoveg[0]);
+}
+function felsorolas(array){
+    return array.join(", ").replace(/,(?=[^,]*$)/," és");
+}
