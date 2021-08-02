789ad5d97c5349bb1cf2bbfb66ef48477ccfeec2
Ambrus <37315575+dkmmtwtw@users.noreply.github.com>
2021-08-02 19:32:45 +0200

    dá

--- public/ja.js
+++ public/ja.js
@@ -1,13 +1,28 @@
-XMLHttpRequest.prototype.setRequestHeaders = function (obj) {
-	Object.keys(obj).forEach(key => {
-		this.setRequestHeader(key, obj[key]);
-	});
-}
-function startsWithVowel(szoveg,magyar){
+
+startsWithVowel = function(szoveg,magyar){
     if magyar
     	return 'aeiouáéíóöőúüű'.includes(szoveg[0]);
     return 'aeiou'.includes(szoveg[0]);
 }
-function felsorolas(array){
+felsorolas = function(array){
     return array.join(", ").replace(/,(?=[^,]*$)/," és");
+}
+serialize = function(obj, prefix) {
+  var str = [],
+    p;
+  for (p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p,
+        v = obj[p];
+      str.push((v !== null && typeof v === "object") ?
+        serialize(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
+XMLHttpRequest.prototype.setRequestHeaders = function (obj) {
+	Object.keys(obj).forEach(key => {
+		this.setRequestHeader(key, obj[key]);
+	});
 }
