6bc891ea4e332fb938554bfdc2c1065e34357f1e
Ambrus <37315575+dkmmtwtw@users.noreply.github.com>
2021-08-07 14:10:25 +0200

    timer

--- public/ja.js
+++ public/ja.js
@@ -26,3 +26,25 @@ XMLHttpRequest.prototype.setRequestHeaders = function (obj) {
 		this.setRequestHeader(key, obj[key]);
 	});
 }
+function Timer(fn, t) {
+    var timerObj = setInterval(fn, t);
+
+    this.stop = function() {
+        if (timerObj) {
+            clearInterval(timerObj);
+            timerObj = null;
+        }
+        return this;
+    }
+    this.start = function() {
+        if (!timerObj) {
+            this.stop();
+            timerObj = setInterval(fn, t);
+        }
+        return this;
+    }
+    this.reset = function(newT = t) {
+        t = newT;
+        return this.stop().start();
+    }
+}
