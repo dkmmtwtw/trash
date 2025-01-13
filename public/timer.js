327e72c05661767cf8345cdad15806d0023c9b3f
Ambrus <37315575+dkmmtwtw@users.noreply.github.com>
2025-01-13 20:24:29 +0100

    Create timer.js

---
+++ public/timer.js
@@ -0,0 +1,22 @@
+function Timer(fn, t) {
+    var timerObj = setInterval(fn, t);
+
+    this.stop = function() {
+        if (timerObj) {
+            clearInterval(timerObj);
+            timerObj = null;
+        }
+        return this;
+    }
+    this.start = function() {
+        if (!timerObj) {
+            this.stop();
+            timerObj = setInterval(fn, t);
+        }
+        return this;
+    }
+    this.reset = function(newT = t) {
+        t = newT;
+        return this.stop().start();
+    }
+}
